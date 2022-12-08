b409c235fb9b791cefcfbecc9ba3d62402242424
devrazzak <developerrazzak@gmail.com>
2022-12-08 12:01:38 +0600

    change

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,7 @@ export default function Home() {
     <div>
       <h2>This is a next je project</h2>
       <h2>this is another push</h2>
-      <h3>change something</h3>
-      <h3>change something</h3>
-      <h2>hello</h2>
-      <h3>main branch</h3>
-      <h2>This is main branch</h2>
-      <h2>hello</h2>
+      <h3>this is main branch</h3>
     </div>
   );
 }
