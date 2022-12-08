7a85b7cbef6238ccb60d1d75e18e722591aea59b
devrazzak <developerrazzak@gmail.com>
2022-12-08 12:05:42 +0600

    fff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h2>This is a next je project</h2>
-      <h2>this is another push</h2>
-      <h3>change something</h3>
-      <h3>change something</h3>
-      <h2>This is main branch</h2>
-      <h2>This is main branch</h2>
-      <h3>main branch</h3>
-      <h2>This is main branch</h2>
-      <h2>hello</h2>
-    </div>
-  );
+  return <div>this is a branch</div>;
 }
